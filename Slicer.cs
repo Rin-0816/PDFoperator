56103473267e2da2470ae9b5706eaff0db5465c1
Rin-0816 <53923402+Rin-0816@users.noreply.github.com>
2023-06-14 14:51:37 +0900

    例外処理の追加 出力ファイル名を自動で決定（手動も可） 出力完了メッセージの追加

--- Slicer.cs
+++ Slicer.cs
@@ -1,12 +1,13 @@
 ﻿using PdfSharpCore.Pdf;
 using PdfSharpCore.Pdf.IO;
+using System.Diagnostics.Tracing;
 
 namespace PDFoperator
 {
     internal class Slicer
     {
         public void pdf_slicer(int start, int end)
-        {
+        {   
             //ファイルを開く
             OpenFileDialog dialog = new OpenFileDialog();
             dialog.Filter = "PDFファイル(*.pdf)|*.pdf";
@@ -23,7 +24,13 @@ namespace PDFoperator
                     SaveFileDialog saveFileDialog1 = new SaveFileDialog();
                     saveFileDialog1.Filter = "PDFファイル(*.pdf)|*.pdf";
                     saveFileDialog1.Title = "PDFファイルを保存する";
-                    saveFileDialog1.ShowDialog();
+                    saveFileDialog1.FileName = Path.GetFileNameWithoutExtension(dialog.FileName)+"_"+(start+1)+"-"+(end+1)+".pdf";
+                    DialogResult result = saveFileDialog1.ShowDialog();
+                    if(result == DialogResult.Cancel)
+                    {
+                        MessageBox.Show("キャンセルされました。");
+                        return;
+                    }
                     if (saveFileDialog1.FileName != "")
                     {
                         //出力ファイルを開く
@@ -36,12 +43,14 @@ namespace PDFoperator
                         //出力ファイルを保存
                         outputDocument.Save(saveFileDialog1.FileName);
                         outputDocument.Close();
+                        MessageBox.Show("処理が完了しました。");
                     }
                 }
                 else
                 {
                     //エラーメッセージを表示
                     MessageBox.Show("PDFファイルを開けないか、もしくはページ数が不正です。");
+                    return;
                 }
             }
         }
